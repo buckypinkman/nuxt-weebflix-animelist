625103fa319aa5fb154516be0aaf77e52d81696b
buckypinkman <rangga7765@gmail.com>
2021-01-14 17:02:17 +0700

    change grid cols each screen breakpoint

--- pages/TopMovie.vue
+++ pages/TopMovie.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="movies">
     <h1 class="md:text-4xl text-3xl mb-4">Top Anime Movies</h1>
-    <div class="grid grid-cols-2 md:grid-cols-5 gap-6">
+    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
       <anime-list
         v-for="result in $store.state.topMovie"
         :key="result.mal_id"
--- pages/Upcoming.vue
+++ pages/Upcoming.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="upcoming">
     <h1 class="md:text-4xl text-3xl mb-4">Top Upcoming Anime</h1>
-    <div class="grid grid-cols-2 md:grid-cols-5 gap-6">
+    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
       <anime-list
         v-for="result in $store.state.upcoming"
         :key="result.mal_id"
